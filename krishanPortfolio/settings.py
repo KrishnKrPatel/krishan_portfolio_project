e0488304c83bb3c8160bd6af018efb3550f75c1c
Krishan Kumar Patel <patelkrishnk1213@gmail.com>
2024-12-23 22:37:05 +0530

    allowed host added

--- krishanPortfolio/settings.py
+++ krishanPortfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--r=*-z$-&@m#vo@1u7!pg0r=47p66)&lir#r^)849d)14qcg8q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
